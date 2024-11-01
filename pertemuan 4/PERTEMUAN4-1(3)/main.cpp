f5403edf257a458d541f74a163343bcfecc89476
YudaBhirawa <ybhirawa5@gmail.com>
2024-11-01 16:40:01 +0700

    Add files via upload

---
+++ pertemuan 4/PERTEMUAN4-1(3)/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char s2[] = {'H', 'e', 'l', 'l', 'o', '\0'}; // Array karakter, bisa dimodifikasi
+
+    // Mengubah karakter kedua dalam array
+    s2[1] = 'a';
+    s2[2] = 'Y';
+    s2[3] = 'Y';
+    // Menampilkan hasil perubahan
+    cout << "Array karakter s2 setelah modifikasi: " << s2 << endl;
+
+    return 0;
+}
+
+
